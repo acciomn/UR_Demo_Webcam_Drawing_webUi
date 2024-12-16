29ff40440c4280cb3500365a08fc2741a91487aa
JAMA-UR <183118304+JAMA-UR@users.noreply.github.com>
2024-12-16 14:10:08 -0500

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-config_name = 'default'  # or 'development', 'testing', 'production'
-app = create_app(config_name)
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
